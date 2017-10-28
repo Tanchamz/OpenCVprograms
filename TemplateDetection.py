75c6a17685400d18961d6943d2f2d35c1af650b4
sm98 <32795449+sm98@users.noreply.github.com>
2017-10-29 01:47:18 +0530

    Create TemplateDetection.py

---
+++ TemplateDetection.py
@@ -0,0 +1,16 @@
+import numpy as np
+import cv2
+
+img_bgr=cv2.imread('<location of image>')
+img gray=cv2.cvtColor(img_bgr,cv2.COLOR_BGR2GRAY)
+
+template=cv2.imread('<location of template>',0)
+w,h=template.shape[::-1]
+
+result=cv2.matchTemplate(img gray,template,cv2.TM_NORMED)
+threshold=0.6
+location=np.where(result>=threshold)
+for pt in zip(*location)
+    cv2.rectangle(img bgr,pt,(pt[0]+w,pt[1]+h),(0,250,250),2)
+cv2.imshow('detected',img bgr)
+cv2.waitKey(0)

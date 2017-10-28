9d4a30c18e9fa4a7e1c64966b6993cff3f2776b2
sm98 <32795449+sm98@users.noreply.github.com>
2017-10-29 02:14:01 +0530

    Create OpenCamera.py

---
+++ OpenCamera.py
@@ -0,0 +1,12 @@
+import numpy as np
+import cv2
+cam=cv2.VideoCapture(0)
+while True :
+    ret,frame=cam.read()
+    if ret==True:
+        cv2.imshow('Original',frame)
+        k=cv2.waitKey(5)& 0xFF
+        if k==27:
+            break
+cap.release()
+cv2.destroyAllWindows()
